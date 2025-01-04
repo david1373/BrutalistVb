fd9d4ca26a872b262f9924ab58cfdb6f6ad51c40
WDevs <127317845+david1373@users.noreply.github.com>
2025-01-04 16:35:54 -0500

    feat: Add Leibal to scraper sources

--- scraper/scrapers/__init__.py
+++ scraper/scrapers/__init__.py
@@ -1,4 +1,5 @@
 from scraper.scrapers.dezeen import scrape_dezeen
+from scraper.scrapers.leibal import scrape_leibal
 
 def scrape_all_sources():
     """Run all scrapers and combine results."""
@@ -6,5 +7,6 @@ def scrape_all_sources():
     
     # Add results from each scraper
     results.extend(scrape_dezeen())
+    results.extend(scrape_leibal())
     
     return results
